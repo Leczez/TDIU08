4a1360020b5773428a2f165218a9bcb30fda51a3
lincl896 <linus.classon@hotmail.com>
2019-11-05 09:22:42 +0100

    2 out of 13

--- C++/Lab0/main.cpp
+++ C++/Lab0/main.cpp
@@ -1,10 +1,30 @@
-ecjhvvcdgh
-dscedc
-hhhc
-hello
-Hej linus
-LIIIIINUS JAG SER DIG
-Hej KENNETH.
-Linus sluta jag vill inte
+#include <iostream>
 
-Jag sitter hemma och stalkar dig KENNETH!
+
+using namespace std;
+int main()
+{
+
+int number;
+
+cout << "Please type in a number: ";
+cin >> number;
+cout << "You typed in the number: " << number << "\n" << flush;
+
+cout << "Please type in 5 numbers: ";
+cin >> number;
+cout << "You typed in the numbers: " << number;
+for(int i{}; i < 4; i++)
+  {
+    cin >> number;
+    cout << " " << number;
+  }
+
+
+
+
+
+
+
+  return 0;
+}
