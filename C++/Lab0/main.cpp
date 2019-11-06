29433d45a3e50973518eea6123f1f18151c98429
lincl896 <linus.classon@hotmail.com>
2019-11-06 17:07:04 +0100

    kinda done

--- C++/Lab0/main.cpp
+++ C++/Lab0/main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <iomanip>
-
 using namespace std;
+
+  void Clear_Trash()
+    {
+      string trash{};
+      getline(cin,trash);
+    }
+
 int main()
 {
   int number;
   double number2;
-/*
+  char character{};
+  string word{};
 
-
-
+  /*
   cout << "Skriv in ett heltal: ";
   cin >> number;
-  cout << "Du skrev in heltalet: " << number << "\n" << flush;
+  Clear_Trash();
+  cout << "Du skrev in heltalet: " << number << endl;
 
   cout << "Skriv in 5 heltal: ";
   cin >> number;
@@ -22,22 +29,68 @@ int main()
     cin >> number;
     cout << " " << number;
   }
+  Clear_Trash();
   cout << endl;
-
 
   cout << "Skriv in ett flyttal: ";
   cin >> number2;
-  cout << "Du skrev in flyttalet: " << fixed << setprecision(1) << number2 << endl;
+  cout << "Du skrev in flyttalet: " << fixed << setprecision(3) << number2 << endl;
+  Clear_Trash();
+
   cout << "Skriv in ett heltal och ett flyttal: ";
   cin >> number >> number2;
   cout << "Du skrev in heltalet: " << setw(8) << number << "\n" << flush;
   cout << "Du skrev in flyttalet: " << fixed << setw(7) << setprecision(4) << number2 << endl;
-*/
+  Clear_Trash();
 
   cout << "Skriv in ett flyttal och ett heltal: ";
   cin >> number2 >> number;
-  cout << "Du skrev in heltalet: " << setfill('-') << setw(9) << number << endl;
+  cout << "Du skrev in heltalet:" << setfill('-') << setw(10) << number << endl;
   cout << "Du skrev in flyttalet:" << fixed <<setprecision(4) << setfill('-') << setw(9) << number2 << endl;
+  Clear_Trash();
 
-return 0;
+  cout << "Skriv in ett tecken: ";
+  cin >> character;
+  cout << "Du skrev in tecknet: " << character << "\n" <<flush;
+  Clear_Trash();
+
+  cout << "Skriv in ett ord: ";
+  cin >> word;
+  cout << "Du skrev in ordet: " << word << endl;
+  Clear_Trash();
+
+  cout << "Skriv in ett heltal och ett ord: ­";
+  cin >> number >> word;
+  cout << "Du skrev in talet |" << number << "| och ordet |" << word << "|." << endl;
+  Clear_Trash();
+
+  cout << "Skriv in ett tecken och ett ord: ";
+  cin >> character >> word;
+  cout << "Du skrev in " << '"' << word << '"' << " och " << "'" << character << "'." << endl;
+  Clear_Trash();
+
+  cout << "Skriv in ett ord och ett tecken: ";
+  cin >> word >> character;
+  cout << "Du skrev in " << '"' << word << '"' << " och " << "'" << character << "'." << endl;
+  Clear_Trash();
+  */
+  cout << "Skriv in en textrad: ";
+  getline(cin,word);
+  cout << "Du skrev in " << '"' << word << '"' << '.' << endl;
+  Clear_Trash();
+
+  cout << "Skriv in en till textrad: ";
+  getline(cin,word);
+  cout << "Du skrev in " << '"' << word << '"' << '.' << endl;
+  Clear_Trash();
+
+  cout << "Skriv in en rad med tre ord: ";
+  cin >> word;
+  cout << "Du skrev in: " << '"' << word << '"' << ", ";
+  cin >> word;
+  cout << '"' << word << '"' << " och ";
+  cin >> word;
+  cout << '"' << word << '"' << '.'
+
+  return 0;
 }
