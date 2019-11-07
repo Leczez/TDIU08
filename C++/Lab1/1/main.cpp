cd0fade1c6d2c5b1a186b1a01e11aadf999492ad
lincl896 <linus.classon@hotmail.com>
2019-11-07 18:13:20 +0100

    c++ the dark code returns

--- C++/Lab1/1/main.cpp
+++ C++/Lab1/1/main.cpp
@@ -3,58 +3,57 @@
 
 using namespace std;
 
-
-  void printtable(int celsius, int kelvin, int fahrenheit, int reaumur, int starting_value, int final_value)
+  void Clear_Trash()
   {
+    string trash{};
+    getline(cin,trash);
+  }
+
+  void printtable(double starting_value, double final_value)
+  {
+    double celsius{starting_value};
+    double kelvin{};
+    double fahrenheit{};
+    double reaumur{};
+
     cout << "Celsius     Kelvin  Farenheit   Réaumur" << endl
          << "---------------------------------------" << endl;
-    for(int i = starting_value; i < final_value; i++)
+    for(int i = starting_value; i <= final_value; i++)
     {
-      
-      cout << celsius << "     " << kelvin << "      " << fahrenheit << "      " << reaumur << endl;
+      kelvin = celsius + 273.15;
+      fahrenheit = celsius * 1.8 + 32;
+      reaumur = celsius * 0.8;
+      cout << setw(7) << setprecision(0) << celsius << fixed << setprecision(2) << setw(11) << kelvin << setw(11) << fahrenheit << setw(10) << reaumur << endl;
+      celsius++;
     }
 
 
   }
 
-  int starting_value{};
-  int final_value{};
-
-  int celsius{0};
-  double kelvin{};
-  int fahrenheit{};
-  int reaumur{};
+  double starting_value{};
+  double final_value{};
 
 int main()
 {
   cout << "Ange startvärde:  ";
   cin >> starting_value;
+  Clear_Trash();
 
-  bool Q{true};
-  while(Q)
+  for(;;)
   {
     cout << "Ange slutvärde:   ";
     cin >> final_value;
+    Clear_Trash();
     if(final_value <= starting_value)
     {
       cout << "Felaktigt slutvärde!" << endl;
-    } else
+    }
+    else
     {
-      Q = false;
+      break;
     }
   }
-  cout << "celsius     kelvin  Farenheit   Réaumur" << endl;
-  cout << "---------------------------------------" << endl;
-  while(celsius <= final_value)
-  {
-      kelvin = celsius + 273.15;
-      cout << setw(7) << celsius << setw(11) << kelvin << endl;
-      celsius++;
-  }
-
-
-
-
+  printtable(starting_value,final_value);
 
   return 0;
 }
