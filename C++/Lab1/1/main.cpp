9eec013264f82e95986ac824a3cfeae4ed682a11
lincl896 <linus.classon@hotmail.com>
2019-11-07 14:06:17 +0100

    c++ begins

--- C++/Lab1/1/main.cpp
+++ C++/Lab1/1/main.cpp
@@ -3,11 +3,32 @@
 
 using namespace std;
 
+  int starting_value{};
+  int final_value{};
 
+  int celsius{};
+  int kelvin{};
+  int fahrenheit{};
+  int reaumur{};
 
 int main()
 {
   cout << "Ange startvärde:  ";
+  cin >> starting_value;
+
+  for(;;)
+  {
+    cout << "Ange slutvärde:   ";
+    cin >> final_value;
+    if(final_value >= starting_value)
+    {
+      break;
+    }else
+    {
+     cout << "Felaktigt slutvärde!" << endl;
+    }
+  }
+
 
 
 
