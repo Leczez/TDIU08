8f345e1cdfa8baeb17e18dc8bb7844bf75cc03d4
lincl896 <linus.classon@hotmail.com>
2019-11-11 12:04:27 +0100

    word count done

--- C++/Lab1/2/main.cpp
+++ C++/Lab1/2/main.cpp
@@ -27,12 +27,59 @@ using namespace std;
   }
 
 
+  int Amount_Of_Words(string text)
+  {
+    int word_count{};
+    for(int i{}; i <= counter; i++)
+    {
+      if(text[i] == ' ' or text[i] == '\n' or (i == 0 and text[i] != '\0'))
+      {
+        word_count++;
+      }
+    }
+    return word_count;
+  }
+
+  string Shortest_Word(string text)
+  {
+  string short_word{};
+  string temp{};
+
+  for(int i{}; i <= counter; i++)
+  {
+    if((text[i] != '\n' and text[i] != '\0') or text[i] != ' ')
+    {
+      cout << text[i] << endl;
+      temp.append(1,text[i]);
+    }
+    else
+    {
+      if(short_word.empty())
+      {
+        cout << "is empty" << endl;
+        short_word = temp;
+        temp.clear();
+      }else if(short_word.size() > temp.size())
+      {
+        cout << "Comparison" << endl;
+        short_word = temp;
+        temp.clear();
+      }
+    }
+  }
+  cout << "Shortest Word: " << short_word << endl;
+  return short_word;
+
+  }
 
   void Analyze_Data(string text)
   {
-
-
-    cout << counter << endl;
+    int word_count{};
+    string shortest_word{};
+    word_count = Amount_Of_Words(text);
+    shortest_word = Shortest_Word(text);
+    //cout << counter << endl;
+    cout << "Texten innehöll " << word_count << " ord." << endl;
   }
 
 
