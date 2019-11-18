c08469e2dc8bc8d47cff13fe7e27e16f755e01a9
lincl896 <linus.classon@hotmail.com>
2019-11-18 12:02:21 +0100

    lab1 part 3 done

--- C++/Lab1/3/main.cpp
+++ C++/Lab1/3/main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 #include <iomanip>
+#include <cctype>
 
 using namespace std;
 
 int main()
 {
+  int alpha{};
+  int numbers{};
+  int whitespaces{};
+  int total_amount_of_characters{};
 
+  char character{};
+  while(cin >> character)
+  {
+    if(isalpha(character))
+    {
+      alpha++;
+    }
+    else if(isblank(character))
+    {
+      whitespaces++;
+    }
+    else if(isdigit(character))
+    {
+      numbers++;
+    }
 
+    total_amount_of_characters++;
+  }
 
+  cout << "Indata innehöll:" << endl <<
+          "Alfabetiska tecken: " << alpha << endl <<
+          "Siffertecken......: " << numbers << endl <<
+          "Vita tecken.......: " << whitespaces << endl <<
+          "Tecken totalt.....: " << total_amount_of_characters << endl;
 
   return 0;
 }
