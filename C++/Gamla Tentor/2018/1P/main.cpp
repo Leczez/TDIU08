18147a9970238fc7d96d20ef8cc2df9079665661
lincl896 <linus.classon@hotmail.com>
2019-12-05 18:30:50 +0100

    now it works!

--- C++/Gamla Tentor/2018/1P/main.cpp
+++ C++/Gamla Tentor/2018/1P/main.cpp
@@ -5,16 +5,17 @@
 using namespace std;
 
 void print_head(int size);
-void print_spikes(int amount);
+void print_spikes(vector<int> sizes);
 vector<int> get_sizes(int amount);
+int find_biggest(vector<int> sizes);
 
 int main()
 {
-  int amount_of_spikes{};
+  int amount_of_spikes{10};
   vector<int> spike_size{};
   print_head(20);
   spike_size = get_sizes(10);
-  //print_spikes(amount_of_spikes);
+  print_spikes(spike_size);
 
 
   return 0;
@@ -38,11 +39,73 @@ vector<int> get_sizes(int amount)
 {
   random_device rd;
   uniform_int_distribution<int> unif(5, 15);
-  mt19937 rand(rd());
+  mt19937 rnd(rd());
   vector<int> spikes{};
+  int buffer{};
   for(int i{}; i < amount; i++)
   {
-    spikes.push_back(rand());
+    buffer = unif(rnd);
+    cout << "Buffer = " << buffer << endl;
+    spikes.push_back(buffer);
   }
   return spikes;
+}
+
+void print_spikes(vector<int> sizes)
+{
+  int biggest{};
+  biggest = find_biggest(sizes);
+  int counter{};
+  cout << "biggest = " << biggest << endl;
+
+  while(counter <= biggest)
+  {
+    //cout << "counter = " << counter << endl;
+    for(int i{}; i < sizes.size(); i++)
+    {
+      //cout << "i = " << i << endl;
+      //cout << " size = " << sizes[i];
+      //cout << " sos ";
+      if(sizes[i] == 0)
+      {
+        cout << "v";
+        if(counter == biggest)
+        {
+          cout << "*";
+        }
+        else
+        {
+          cout << " ";
+        }
+      }
+      else if(sizes[i] > 0)
+      {
+        cout << "|";
+        cout << " ";
+      }
+      else if(sizes[i] < 0)
+      {
+        cout << "  ";
+      }
+        sizes[i] += -1;
+    }
+    cout << endl;
+    counter++;
+  }
+}
+
+int find_biggest(vector<int> sizes)
+{
+
+  int biggest{};
+
+  for(int i : sizes)
+  {
+    cout << "I = " << i << endl;
+    if(biggest < i)
+    {
+      biggest = i;
+    }
+  }
+  return biggest;
 }
